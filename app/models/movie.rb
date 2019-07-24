5629521abb1fe14c8d9927f135ac239d1228dece
Trung <nguyenvantrung2015@gmail.com>
2019-07-24 15:42:57 +0700

    Init admin dashboard

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,8 +2,8 @@ class Movie < ApplicationRecord
   has_one :medium, as: :reviewable, dependent: :destroy
   mount_uploader :poster, PosterUploader
 
-  scope :create_top_new, ->{order release_date: :desc }
-  scope :create_top_score, ->{order critic_score: :desc }
+  scope :create_top_new, ->{order release_date: :desc}
+  scope :create_top_score, ->{order critic_score: :desc}
 
   ATTR = %i(name release_date critic_score audience_score info poster).freeze
 
