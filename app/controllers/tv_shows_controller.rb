846da3af11bdefde1b5e70fea15011b7eab0e7c3
Trung <nguyenvantrung2015@gmail.com>
2019-07-26 14:25:51 +0700

    GUI TVshow Page + fill data

--- app/controllers/tv_shows_controller.rb
+++ app/controllers/tv_shows_controller.rb
@@ -1,9 +1,20 @@
 class TvShowsController < ApplicationController
-  before_action :load_tvshow, except: %i(index new create)
+  before_action :load_tvshow, only: %i(index show)
+  before_action :build_user, only: %i(index show)
 
-  def index; end
+  def index
+    @tv_shows = TvShow.create_desc.page(params[:page]).per Settings.tvshows
+                                                                   .paginate
+  end
 
-  def show; end
+  def show
+    @tv_shows = TvShow.create_desc
+    @top_new_show = Movie.create_top_new.top_new_show
+    @top_new_more = @top_new_show.top_new_more
+
+    @top_score_show = Movie.create_top_score.top_score_show
+    @top_score_more = @top_score_show.top_score_more
+  end
 
   private
 
@@ -13,5 +24,9 @@ class TvShowsController < ApplicationController
     return if @tv_show
     flash[:danger] = t ".not_found"
     redirect_to tv_shows_url
+  end
+
+  def build_user
+    @user = User.new
   end
 end
