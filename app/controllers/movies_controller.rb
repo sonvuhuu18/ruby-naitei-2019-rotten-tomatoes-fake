5c34af1ed71dd002d449e790405b7fe47075ee5a
trungnv <nguyenvantrung2016@gmail.com>
2019-08-21 10:40:40 +0700

    final-2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,11 +10,11 @@ class MoviesController < ApplicationController
   def show
     @review = Review.new
 
-    @critic_score = @movie.critic_score
-    @critic_score = @critic_score.zero? ? "N/A" : @critic_score.round(1)
-
-    @audience_score = @movie.audience_score
-    @audience_score = @audience_score.zero? ? "N/A" : @audience_score.round(1)
+    @critic_score = @movie.score(:critic)
+                        .zero? ? "N/A" : @movie.score(:critic).round(1)
+    @audience_score = @movie.score(:normal)
+                          .zero? ? "N/A" : @movie.score(:normal).round(1)
+    @celebrities = Movie.celebrities_list @movie.id
   end
 
   private
@@ -22,22 +22,16 @@ class MoviesController < ApplicationController
   def load_movie
     @movie = Movie.find_by id: params[:id]
 
-    if @movie
-      @review = Review.new
-      @critic_score = @movie.score :critic
-      @audience_score = @movie.score :normal
-      @celebrities = Movie.celebrities_list @movie.id
-    else
-      flash[:danger] = t ".not_found"
-      redirect_to movies_url
-    end
+    return if @movie
+    flash[:danger] = t ".not_found"
+    redirect_to movies_url
   end
 
   def build_movie_tvshow
     @top_score_movie = Movie.create_top_score
-    @top_score_movie_tab = @top_score_movie.take 15
+    @top_score_movie_tab = @top_score_movie.take Settings.tab_show
 
-    @top_score_tvshow = TvShow.all.create_top_score
-    @top_score_tvshow_tab = @top_score_tvshow.take 15
+    @top_score_tvshow = TvShow.create_top_score
+    @top_score_tvshow_tab = @top_score_tvshow.take Settings.tab_show
   end
 end
