c5dc302e5db4ae9aaa0462a30f060a45f2638f4b
trungnv <nguyenvantrung2016@gmail.com>
2019-08-21 10:40:40 +0700

    final-2

--- app/models/tv_show.rb
+++ app/models/tv_show.rb
@@ -1,36 +1,36 @@
 class TvShow < ApplicationRecord
   include Search
   has_many :seasons, dependent: :destroy
-  has_many :episodes, through: :seasons
-  has_many :medium, through: :episodes
-  has_many :celebrity_media, through: :medium
-  has_many :celebrities, through: :celebrity_media
 
   mount_uploader :poster, TvShowPosterUploader
 
   scope :create_desc, ->{order(created_at: :desc)}
-  scope :create_top_score, ->{sort_by(&:critic_score).reverse}
+  scope :create_top_score, ->{sort_by{|tv| [tv.score(:critic), tv.score(:normal)]}.reverse}
+
   scope :load_all, (
   lambda do
-    joins(:celebrities).all.select("tv_shows.id, celebrities.name, "\
-    "tv_shows.name as film_name, episodes.release_date")
+    joins(seasons: [episodes: [medium: [celebrity_media: :celebrity]]]).select("tv_shows.id, celebrities.name, "\
+      "tv_shows.name as film_name, episodes.release_date")
   end
   )
   scope :list_search, ->(tvshows_id){find(tvshows_id)}
+
+  scope :celebrities_list, (
+  lambda do |tvshow_id|
+    Celebrity.where(id: includes(seasons: [episodes: [medium: [celebrity_media: :celebrity]]]).where(id: tvshow_id)
+                            .pluck("celebrities.id").uniq)
+  end)
+
   ATTR = %i(name info poster).freeze
 
   validates :name, presence: true,
-    length: {maximum: Settings.tvshows.name_max_length}
+            length: {maximum: Settings.tvshows.name_max_length}
   validates :info, presence: true,
-    length: {maximum: Settings.tvshows.info_max_length}
+            length: {maximum: Settings.tvshows.info_max_length}
 
-  def critic_score
-    arr = seasons.map(&:critic_score).reject(&:zero?)
-    arr ? arr.reduce{|a, e| a + e} / arr.size : 0
-  end
-
-  def audience_score
-    arr = seasons.map(&:audience_score).reject(&:zero?)
-    arr ? arr.reduce{|a, e| a + e} / arr.size : 0
+  def score user_role
+    arr = seasons.map{|s| s.score(user_role)}.reject(&:zero?)
+    return 0 if arr.empty?
+    arr.reduce(:+) / arr.size
   end
 end
